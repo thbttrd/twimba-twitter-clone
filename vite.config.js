ca5e9ed933090da26f6b1c927d0e95c78524259c
thbttrd <thbttrd.dev@pm.me>
2024-06-22 18:30:35 +0200

    minor change to vite.config.js to handle static images

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import {defineConfig} from "vite"
+const { defineConfig } = require('vite');
 
-export default defineConfig({
-	plugins: [
-		
-	]
-})
+export default defineConfig(async () => {
+  const { viteStaticCopy } = await import('vite-plugin-static-copy');
+  
+  return {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'images/*',
+            dest: 'images/'
+          }
+        ]
+      })
+    ]
+  };
+});
